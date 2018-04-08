a4826287a63dc9f87c7bffdec7c91693270cfc89
Iben <iben95@hotmail.dk>
2018-04-08 12:36:44 +0200

    Opdateret

--- join,groupby,orderby.sql
+++ join,groupby,orderby.sql
@@ -1,9 +1,9 @@
 # eksempel på Join og Order by
-SELECT BaneNavn, BygningsNavn,HoldNavn, TrænerNavn, DayCode, StartTime, EndTime
+SELECT BaneNavn, BygningsNavn,HoldNavn, TrænerNavn, Stilling, DayCode, StartTime, EndTime
 FROM TræningsSession 
 join  Holdtræner on  TræningsSession.HoldtrænerID = Holdtræner.HoldtrænerID
 join  Baner on  TræningsSession.BaneID = Baner.BaneID
 join  TimeSlot on  TræningsSession.TimeSlotID = TimeSlot.TimeSlotID
 join Træner on Holdtræner.TrænerID = Træner.TrænerID
 
-order by BaneNavn, BygningsNavn,HoldNavn, TrænerNavn, DayCode, StartTime, EndTime
+order by BaneNavn, BygningsNavn, DayCode, StartTime, EndTime
