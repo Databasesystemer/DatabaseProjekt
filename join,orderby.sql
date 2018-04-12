946bbcb97eb624773c64ae6ad73645ccac80e87d
Sinkjaer <sinkjaermikkel@gmail.com>
2018-04-12 12:14:17 +0200

    Flere order by

--- join,orderby.sql
+++ join,orderby.sql
@@ -6,6 +6,18 @@ join  Baner on  TræningsSession.BaneNavn = Baner.BaneNavn
 join  TimeSlot on  TræningsSession.TimeSlotID = TimeSlot.TimeSlotID
 join Træner on Holdtræner.TrænerID = Træner.TrænerID
 
-order by  BygningsNavn,BaneNavn, HoldNavn,  DayCode, StartTime, EndTime
+order by  BygningsNavn,BaneNavn, HoldNavn,  DayCode, StartTime, EndTime;
 
 # order by har ikke TrænerNavn -> så du ser i forhold til baner 
+
+#  Group by til at finde hvor mange hold en træner har 
+select TrænerNavn, TrænerID, count(Holdtræner.TrænerID) as antalHold
+from Træner NATURAL left outer JOIN  HoldTræner
+group by TrænerNavn, TrænerID;
+
+# Hvilke Events bliver alfholdt i uge 28 og hvor bliver de afholdet henne 
+select EventNavn, DayCode, StartTime, EndTime, BygningsNavn
+From Eventss 
+natural  left outer join TimeSlot
+where Uge = 28
+order by EventNavn;
